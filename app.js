6ced241d0b345e7e23ca3877817d5487625b5289
unknown <amv.harsh@gmail.com>
2023-01-30 17:23:34 +0530

    intitial commit updating site ui

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const data=[
    {
     url:"CM(1).pdf",
-    title:"CM 3rd sem",
+    title:"CM ",
+    sem: "3rd"
    },
    {
     url:"CM(1).pdf",
@@ -16,15 +17,16 @@ const data=[
 
 var s=``;
 data.forEach(function(item,index){
-    s+=`<div class="into">
+    s+=`<a href='${item.url}'>
+    <div class="into">
     <div class="car">
-    <a class="logo" href='${item.url}target="_blank">
-        <h2 class="text">${item.title} </h2>
-        <span class="material-symbols-outlined">
-            folder_open
-        </span></div>
-        </a>
-    </div>`
+    <p class="text">${item.title + " "+ item.sem}</p>
+    <span class="material-symbols-outlined">
+    open_in_new
+    </span>
+    </div>
+    </div>
+    </a>`
 })
 
 $('.link').html(s);
