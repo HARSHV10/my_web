9d4f06bbc688ebb975398c8ed7b82aa1a899c119
unknown <amv.harsh@gmail.com>
2023-01-30 16:59:59 +0530

    first commit

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ var s=``;
 data.forEach(function(item,index){
     s+=`<div class="into">
     <div class="car">
-
+    <a class="logo" href='${item.url}target="_blank">
         <h2 class="text">${item.title} </h2>
-        <a class="logo" href='${item.url}target="_blank"><span class="material-symbols-outlined">
+        <span class="material-symbols-outlined">
             folder_open
-        </span></a></div>
+        </span></div>
+        </a>
     </div>`
 })
 
