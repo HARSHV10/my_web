0fbc0975393f73a7176422245b5fd1553448832d
unknown <amv.harsh@gmail.com>
2023-01-31 23:45:27 +0530

    add script

--- app.js
+++ app.js
@@ -9,7 +9,47 @@ const data=[
     url:"dlcd.pdf",
     title:"DLCD",
     sem:"3rd"
-   }
+   },
+   {
+    url:"oops/2.txt",
+    title:"2.text",
+    sem:"3rd"
+   },
+   {
+    url:"oops/mat.txt",
+    title:"mat",
+    sem:"3rd"
+   },
+   {
+    url:"oops/str.txt",
+    title:"str",
+    sem:"3rd"
+   },
+   {
+    url:"oops/str_2.txt",
+    title:"str_2",
+    sem:"3rd"
+   },
+   {
+    url:"oops/temp.txt",
+    title:"template",
+    sem:"3rd"
+   },
+   {
+    url:"oops/friend.txt",
+    title:"hello_friend",
+    sem:"3rd"
+   },
+   {
+    url:"oops/comp.txt",
+    title:"comp",
+    sem:"3rd"
+   },
+   {
+    url:"oops/virt.txt",
+    title:"virt",
+    sem:"3rd"
+   },
 ]
 {
 
